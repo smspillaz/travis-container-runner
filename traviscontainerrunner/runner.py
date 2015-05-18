89a99cea567d04513aa358a193caf78fc8e55101
Sam Spilsbury <smspillaz@gmail.com>
2015-05-18 09:40:06 +0800

    Keep track of files that would be cached
    This is useful for debugging constantly stale caches

--- traviscontainerrunner/runner.py
+++ traviscontainerrunner/runner.py
@@ -8,6 +8,7 @@
 import os
 import subprocess
 import tempfile
+import sys
 import yaml
 
 _CONTAINER_ONLY_ERROR = ("travis-container-runner only works with "
@@ -15,6 +16,69 @@ _CONTAINER_ONLY_ERROR = ("travis-container-runner only works with "
                          "use of sudo."
                          ""
                          "Add sudo: false to your .travis.yml")
+
+
+class Cache(object):
+
+    """Cache entry."""
+
+    def __init__(self, line):
+        """Initialize this cache entry."""
+        super(Cache, self).__init__()
+
+        components = line.split(" ")
+        self.path = components[0]
+        self.mtime = components[1]
+
+    def __repr__(self):
+        """Represent as string."""
+        return self.path + " " + self.mtime
+
+    def match(self, other):
+        """Compare cache entry against the other checking if it is newer."""
+        if self.path == other.path:
+            return bool(self.mtime <= other.mtime)
+
+        return False
+
+
+def record_cache(travis_yaml):
+    """Record caches."""
+
+    if travis_yaml.get("cache", None):
+        for cache_dir in travis_yaml["cache"].get("directories", list()):
+            try:
+                with open(".travis-runner-memo-" + cache_dir.replace("/", "_"),
+                          "r") as f:
+                    cache_file_entries = set([Cache(l) for l in f.readlines()])
+            except IOError:
+                cache_file_entries = set()
+
+            current_cache_dir_entries = set()
+            for root, _, filenames in os.walk(cache_dir):
+                for filename in filenames:
+                    path = os.path.join(root, filename).replace(" ", "_")
+                    try:
+                        mtime = os.stat(os.path.join(root, filename)).st_mtime
+                    except OSError:
+                        mtime = 1
+                    line = "{0} {1}".format(path, mtime)
+                    current_cache_dir_entries |= set([Cache(line)])
+
+            for current_entry in current_cache_dir_entries:
+                matching_entry_found = False
+
+                for cache_entry in cache_file_entries:
+                    if current_entry.match(cache_entry):
+                        matching_entry_found = True
+                        break
+                if not matching_entry_found:
+                    print("Would cache " + current_entry.path)
+
+            with open(".travis-runner-memo-" + cache_dir.replace("/", "_"),
+                      "w") as f:
+                f.truncate(0)
+                f.write("\n".join([repr(e) for e in current_cache_dir_entries]))
 
 
 def main():
@@ -31,13 +95,17 @@ def main():
 
         script_commands = []
 
-        for candidate_section in ["before_install",
-                                  "install",
-                                  "script",
-                                  "before_cache",
-                                  "after_success",
-                                  "after_script",
-                                  "before_deploy"]:
+        sections = ["before_install",
+                    "install",
+                    "script",
+                    "before_cache"]
+
+        if len(sys.argv) > 1 and sys.argv[1] == "--run-after-steps":
+            sections += ["after_success",
+                         "after_script",
+                         "before_deploy"]
+
+        for candidate_section in sections:
             try:
                 script_commands += (travis_yaml[candidate_section] or list())
             except KeyError:
@@ -50,5 +118,10 @@ def main():
                 script_file.write("{0}\n".format(command))
 
             script_file.flush()
-            return subprocess.check_call(["bash",
-                                          "{0}".format(script_file.name)])
+            result = subprocess.check_call(["bash",
+                                            "{0}".format(script_file.name)])
+
+            if result != 0:
+                return result
+
+            record_cache(travis_yaml)
